debc75e105856ce99e93fa23ed2513a92e4e574f
Abhishek Singh <abhishek.libran2k@gmail.com>
2022-10-15 12:17:46 +0530

    Fixed the typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 streamlit.title('My Parents are at Helathy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Hard Boiled and free range eggs'
+streamlit.text('Hard Boiled and free range eggs')
