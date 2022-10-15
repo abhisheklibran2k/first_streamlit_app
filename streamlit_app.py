8ce677330aacecee39c4ec57a754605d5141f8ec
Abhishek Singh <abhishek.libran2k@gmail.com>
2022-10-15 12:19:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 streamlit.title('My Parents are at Helathy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Hard Boiled and free range eggs')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
